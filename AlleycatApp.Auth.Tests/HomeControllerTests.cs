612b6e687d22f982bd77af61661ca510c8fdcc9a
bkisly <b.kisly@gmail.com>
2023-11-16 12:43:13 +0100

    Added coverage filters

---
+++ AlleycatApp.Auth.Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using AlleycatApp.Auth.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlleycatApp.Auth.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void DoesReturnHelloWorld()
+        {
+            // Arrange
+
+            var controller = new HomeController();
+
+            // Act
+
+            var result = controller.Index() as OkObjectResult;
+
+            // Assert
+
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal("Hello world!", result.Value);
+        }
+    }
+}
