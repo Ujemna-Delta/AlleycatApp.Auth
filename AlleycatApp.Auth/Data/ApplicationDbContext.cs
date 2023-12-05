55baea14e251071b7a6ecd378bb1dba521aa9c13
bkisly <b.kisly@gmail.com>
2023-12-05 18:35:42 +0100

    Added race model

--- AlleycatApp.Auth/Data/ApplicationDbContext.cs
+++ AlleycatApp.Auth/Data/ApplicationDbContext.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AlleycatApp.Auth.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlleycatApp.Auth.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options);
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public virtual DbSet<Race> Races => Set<Race>();
+    }
 }
---
+++ AlleycatApp.Auth/Models/Race.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlleycatApp.Auth.Models
+{
+    public class Race
+    {
+        public int Id { get; set; }
+        [StringLength(64)] public string Name { get; set; } = string.Empty;
+        [StringLength(256)] public string? Description { get; set; }
+        public DateTime BeginTime { get; set; }
+        [StringLength(256)] public string StartAddress { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(8, 2)")] public decimal? ValueModifier { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsFreeOrder { get; set; }
+    }
+}
