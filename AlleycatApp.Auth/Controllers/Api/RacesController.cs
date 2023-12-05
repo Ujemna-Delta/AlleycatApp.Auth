36a11528d6f4b6602aca9d5bec5aef8dbbc5535c
bkisly <b.kisly@gmail.com>
2023-12-05 21:58:23 +0100

    Created controllers for races

---
+++ AlleycatApp.Auth/Controllers/Api/RacesController.cs
@@ -0,0 +1,68 @@
+﻿using AlleycatApp.Auth.Infrastructure.Exceptions;
+using AlleycatApp.Auth.Models;
+using AlleycatApp.Auth.Models.Dto;
+using AlleycatApp.Auth.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlleycatApp.Auth.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RacesController(IRaceRepository raceRepository, IMapper mapper) : ControllerBase
+    {
+        [HttpGet] 
+        public IActionResult GetRaces() => Ok(raceRepository.Entities.ToArray());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRaceById(int id) => Ok(await raceRepository.FindByIdAsync(id));
+
+        [HttpPost]
+        public async Task<IActionResult> AddRace(RaceDto race)
+        {
+            try
+            {
+                var raceModel = mapper.Map<Race>(race);
+                var createdRace = await raceRepository.AddAsync(raceModel);
+                return CreatedAtAction(nameof(AddRace), createdRace);
+            }
+            catch (InvalidModelException e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateRace(int id, RaceDto race)
+        {
+            try
+            {
+                var raceModel = mapper.Map<Race>(race);
+                await raceRepository.UpdateAsync(id, raceModel);
+                return NoContent();
+            }
+            catch (InvalidModelException e)
+            {
+                return BadRequest(e);
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRace(int id)
+        {
+            try
+            {
+                await raceRepository.DeleteAsync(id);
+                return NoContent();
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- AlleycatApp.Auth/Program.cs
+++ AlleycatApp.Auth/Program.cs
@@ -1,4 +1,5 @@
 using AlleycatApp.Auth.Data;
+using AlleycatApp.Auth.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,10 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddScoped<IRaceRepository, RaceDbRepository>();
 
 var app = builder.Build();
 
