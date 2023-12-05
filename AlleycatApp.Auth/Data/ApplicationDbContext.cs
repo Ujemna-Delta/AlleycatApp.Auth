198595886c757391c90661c4e9b0cf37b43f0092
bkisly <b.kisly@gmail.com>
2023-12-05 18:23:22 +0100

    Created DbContext

---
+++ AlleycatApp.Auth/Data/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AlleycatApp.Auth.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options);
+}
--- AlleycatApp.Auth/Program.cs
+++ AlleycatApp.Auth/Program.cs
@@ -1,6 +1,12 @@
+using AlleycatApp.Auth.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
