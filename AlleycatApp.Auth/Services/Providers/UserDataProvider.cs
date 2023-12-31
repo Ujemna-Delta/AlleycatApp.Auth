619482ed8015c9eeae3ecd2ba28064ee7c701f1d
bkisly <b.kisly@gmail.com>
2023-12-31 02:28:02 +0100

    Modified account service to handle all types of users

--- AlleycatApp.Auth/Services/Providers/UserDataProvider.cs
+++ AlleycatApp.Auth/Services/Providers/UserDataProvider.cs
@@ -1,11 +1,21 @@
 ﻿using System.Security.Claims;
+using AlleycatApp.Auth.Infrastructure;
+using AlleycatApp.Auth.Models.Users;
 using Microsoft.AspNetCore.Identity;
 
 namespace AlleycatApp.Auth.Services.Providers
 {
-    public class UserDataProvider(UserManager<IdentityUser> userManager) : IUserDataProvider
+    public class UserDataProvider(IUserServicesProvider userServicesProvider) : IUserDataProvider
     {
         public string? GetUserIdForClaimsPrincipal(ClaimsPrincipal principal)
-            => userManager.GetUserId(principal);
+            => userServicesProvider.DefaultManager.GetUserId(principal);
+
+        public string? GetRoleName<TUser>() where TUser : IdentityUser, new() => new TUser() switch
+        {
+            Manager => Constants.RoleNames.Manager,
+            Pointer => Constants.RoleNames.Pointer,
+            Attendee => Constants.RoleNames.Attendee,
+            _ => null
+        };
     }
 }
