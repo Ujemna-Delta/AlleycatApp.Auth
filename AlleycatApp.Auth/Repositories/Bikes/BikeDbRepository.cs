a18f6a2c3f642746d59d9d1aedb5336ec76304ac
bkisly <b.kisly@gmail.com>
2024-01-04 12:53:59 +0100

    Created bikes repository

---
+++ AlleycatApp.Auth/Repositories/Bikes/BikeDbRepository.cs
@@ -0,0 +1,21 @@
+﻿using AlleycatApp.Auth.Data;
+using AlleycatApp.Auth.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlleycatApp.Auth.Repositories.Bikes
+{
+    public class BikeDbRepository(ApplicationDbContext context, IMapper mapper) : CrudDbRepository<Bike, int>(context, mapper), IBikeRepository
+    {
+        public override IQueryable<Bike> Entities => context.Bikes;
+
+        public async Task<IEnumerable<Bike>> GetBikesByUserIdAsync(string userId)
+            => await Entities.Where(b => b.AttendeeId == userId).ToArrayAsync();
+
+        public override async Task DeleteAsync(int id)
+        {
+            context.Bikes.Remove(await FindByIdStrictAsync(id));
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ AlleycatApp.Auth/Repositories/Bikes/IBikeRepository.cs
@@ -0,0 +1,9 @@
+﻿using AlleycatApp.Auth.Models;
+
+namespace AlleycatApp.Auth.Repositories.Bikes
+{
+    public interface IBikeRepository : ICrudRepository<Bike, int>
+    {
+        Task<IEnumerable<Bike>> GetBikesByUserIdAsync(string userId);
+    }
+}
