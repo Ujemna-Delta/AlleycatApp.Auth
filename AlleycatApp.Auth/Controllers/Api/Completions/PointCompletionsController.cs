64ac43d4977ab7f2e6d7d896909e078a97a52062
bkisly <b.kisly@gmail.com>
2024-01-04 21:04:01 +0100

    Created point completion controller

---
+++ AlleycatApp.Auth/Controllers/Api/Completions/PointCompletionsController.cs
@@ -0,0 +1,80 @@
+﻿using AlleycatApp.Auth.Infrastructure.Exceptions;
+using AlleycatApp.Auth.Models.Dto;
+using AlleycatApp.Auth.Models;
+using AlleycatApp.Auth.Repositories.Points;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlleycatApp.Auth.Controllers.Api.Completions
+{
+    [Route("api/completions/points")]
+    [ApiController]
+    public class PointCompletionsController(IPointCompletionRepository repository, IMapper mapper) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetPointCompletions() =>
+            Ok(await repository.Entities.Select(r => mapper.Map<PointCompletionDto>(r)).ToArrayAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPointCompletionById(int id)
+        {
+            var completion = await repository.FindByIdAsync(id);
+            return completion != null ? Ok(completion) : NotFound();
+        }
+
+        [HttpGet("attendee/{id}")]
+        public async Task<IActionResult> GetPointCompletionsByUserId(string id) =>
+            Ok(await repository.GetByUserIdAsync(id));
+
+        [HttpGet("point/{id}")]
+        public async Task<IActionResult> GetPointCompletionsByRaceId(int id) =>
+            Ok(await repository.GetByPointIdAsync(id));
+
+        [HttpPost]
+        public async Task<IActionResult> AddPointCompletion(PointCompletionDto pointCompletionDto)
+        {
+            try
+            {
+                var result = await repository.AddAsync(mapper.Map<PointCompletion>(pointCompletionDto));
+                return CreatedAtAction(nameof(AddPointCompletion), mapper.Map<PointCompletionDto>(result));
+            }
+            catch (InvalidModelException e)
+            {
+                return BadRequest(e.ModelError);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePointCompletion(int id, PointCompletionDto pointCompletionDto)
+        {
+            try
+            {
+                await repository.UpdateAsync(id, mapper.Map<PointCompletion>(pointCompletionDto));
+                return NoContent();
+            }
+            catch (InvalidModelException e)
+            {
+                return BadRequest(e.ModelError);
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePointCompletion(int id)
+        {
+            try
+            {
+                await repository.DeleteAsync(id);
+                return NoContent();
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
