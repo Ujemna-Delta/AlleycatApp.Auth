293da94ac4cdab89c47b7179e71f01b0daa58297
bkisly <b.kisly@gmail.com>
2024-01-04 12:47:34 +0100

    Created bike model

---
+++ AlleycatApp.Auth/Infrastructure/Profiles/BikeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AlleycatApp.Auth.Models.Dto;
+using AlleycatApp.Auth.Models;
+using AutoMapper;
+
+namespace AlleycatApp.Auth.Infrastructure.Profiles
+{
+    public class BikeProfile : Profile
+    {
+        public BikeProfile()
+        {
+            CreateMap<Bike, BikeDto>();
+            CreateMap<BikeDto, Bike>().ForMember(r => r.Id, opt => opt.Ignore());
+            CreateMap<Bike, Bike>().ForMember(r => r.Id, opt => opt.Ignore());
+        }
+    }
+}
