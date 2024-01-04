b32075b7d6178605b61949025020d76ef82105c0
bkisly <b.kisly@gmail.com>
2024-01-04 12:09:44 +0100

    Created tasks controller

---
+++ AlleycatApp.Auth/Controllers/Api/TasksController.cs
@@ -0,0 +1,76 @@
+﻿using AlleycatApp.Auth.Infrastructure.Exceptions;
+using AlleycatApp.Auth.Models;
+using AlleycatApp.Auth.Models.Dto;
+using AlleycatApp.Auth.Repositories.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlleycatApp.Auth.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController(ITaskRepository repository, IMapper mapper) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetTasks() =>
+            Ok(await repository.Entities.Select(t => mapper.Map<TaskDto>(t)).ToArrayAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTaskById(int id)
+        {
+            var point = await repository.FindByIdAsync(id);
+            return point != null ? Ok(mapper.Map<TaskDto>(point)) : NotFound();
+        }
+
+        [HttpGet("point/{id}")]
+        public async Task<IActionResult> GetTasksByPointId(int id) =>
+            Ok((await repository.GetByPointId(id)).Select(mapper.Map<TaskDto>));
+
+        [HttpPost]
+        public async Task<IActionResult> AddTask(TaskDto taskDto)
+        {
+            try
+            {
+                var result = await repository.AddAsync(mapper.Map<TaskModel>(taskDto));
+                return CreatedAtAction(nameof(AddTask), mapper.Map<TaskDto>(result));
+            }
+            catch (InvalidModelException e)
+            {
+                return BadRequest(e.ModelError);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTask(int id, TaskDto taskDto)
+        {
+            try
+            {
+                await repository.UpdateAsync(id, mapper.Map<TaskModel>(taskDto));
+                return NoContent();
+            }
+            catch (InvalidModelException e)
+            {
+                return BadRequest(e.ModelError);
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            try
+            {
+                await repository.DeleteAsync(id);
+                return NoContent();
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
