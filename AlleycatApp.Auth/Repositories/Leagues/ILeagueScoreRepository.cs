79e7abda5769d4af7b7d5a6bdda8cb34b0000972
bkisly <b.kisly@gmail.com>
2024-01-12 19:14:06 +0100

    Implemented league score repository

---
+++ AlleycatApp.Auth/Repositories/Leagues/ILeagueScoreRepository.cs
@@ -0,0 +1,10 @@
+﻿using AlleycatApp.Auth.Models;
+
+namespace AlleycatApp.Auth.Repositories.Leagues
+{
+    public interface ILeagueScoreRepository : ICrudRepository<LeagueScore, int>
+    {
+        Task<IEnumerable<LeagueScore>> GetByUserIdAsync(string userId);
+        Task<IEnumerable<LeagueScore>> GetByLeagueIdAsync(short leagueId);
+    }
+}
---
+++ AlleycatApp.Auth/Repositories/Leagues/LeagueScoreDbRepository.cs
@@ -0,0 +1,24 @@
+﻿using AlleycatApp.Auth.Data;
+using AlleycatApp.Auth.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlleycatApp.Auth.Repositories.Leagues
+{
+    public class LeagueScoreDbRepository(ApplicationDbContext context, IMapper mapper) : CrudDbRepository<LeagueScore, int>(context, mapper), ILeagueScoreRepository
+    {
+        public override IQueryable<LeagueScore> Entities => context.LeagueScores;
+
+        public async Task<IEnumerable<LeagueScore>> GetByUserIdAsync(string userId) =>
+            await Entities.Where(l => l.AttendeeId == userId).ToArrayAsync();
+
+        public async Task<IEnumerable<LeagueScore>> GetByLeagueIdAsync(short leagueId) =>
+            await Entities.Where(l => l.LeagueId == leagueId).ToArrayAsync();
+
+        public override async Task DeleteAsync(int id)
+        {
+            context.LeagueScores.Remove(await FindByIdStrictAsync(id));
+            await context.SaveChangesAsync();
+        }
+    }
+}
